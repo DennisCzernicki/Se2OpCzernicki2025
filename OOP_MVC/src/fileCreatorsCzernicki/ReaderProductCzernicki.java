611749eaa4d6a06f270a17d03520f6477ab00b67
DennisCzernicki <dennis.czernicki@stud.hs-bochum.de>
2025-12-13 12:57:10 +0100

    Delete OOP_MVC directory

--- OOP_MVC/src/fileCreatorsCzernicki/ReaderProductCzernicki.java
+++
@@ -1,9 +0,0 @@
-package fileCreatorsCzernicki;
-
-import java.io.IOException;
-
-public abstract class ReaderProductCzernicki {
-	public abstract String[] leseAusDatei() throws IOException;
-	public abstract void schliesseDatei() throws IOException;
-
-}
--- OOP_MVC/src/module-info.java
+++
@@ -1,6 +0,0 @@
-module OOP_MVC {
-	requires javafx.graphics;
-	requires javafx.controls;
-	
-	exports main;
-}
